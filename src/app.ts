96c62c71d2241563646f2487abf2c4222060bd83
Dax0s <dev@dklimavicius.com>
2024-11-13 00:13:23 +0200

    Remove node modules and install eslint and prettier with husky

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 3001;
 
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello World" });
+  res.status(200).json({ message: "Hello World" });
 });
 
 app.listen(port, () => {
-    console.log(`App listening on http://localhost:${port}`);
+  console.log(`App listening on http://localhost:${port}`);
 });
