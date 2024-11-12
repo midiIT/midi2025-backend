3528452c4b2d3bdd6039e51388fb51e1119b1570
Dax0s <dev@dklimavicius.com>
2024-11-13 01:03:14 +0200

    Add sequelize

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,84 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { DataTypes, Model, Sequelize } from "sequelize";
+import process from "process";
 
 const app = express();
-const port = 3001;
+const router = express.Router();
+const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World" });
+const localhostOnly = (req: Request, res: Response, next: NextFunction) => {
+  const ip = req.ip;
+  if (ip === "127.0.0.1" || ip === "::1" || ip === "::ffff:127.0.0.1") {
+    next();
+  } else {
+    res.status(403).json({ error: "Access restricted to localhost" });
+  }
+};
+
+router.use(localhostOnly);
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./database.sqlite",
 });
+
+class Tracking extends Model {}
+Tracking.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true, // Add this
+      defaultValue: DataTypes.UUIDV4, // Optional: auto-generates UUID
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    clicks: DataTypes.BIGINT,
+  },
+  { sequelize, modelName: "Tracking" },
+);
+
+void sequelize.sync();
+
+router.get("/tracking", async (req, res) => {
+  const trackingUrls = await Tracking.findAll();
+  res.json(trackingUrls);
+});
+
+router.post("/tracking/:name", async (req, res) => {
+  try {
+    const trackingUrl = await Tracking.create({
+      name: req.params.name,
+      clicks: 0,
+    });
+
+    res.status(201).json(trackingUrl);
+  } catch {
+    res.status(409).json({});
+  }
+});
+
+router.put("/tracking/:name", async (req, res) => {
+  try {
+    const trackingUrl = await Tracking.findOne({
+      where: { name: req.params.name },
+    });
+
+    if (trackingUrl) {
+      await trackingUrl.update({ clicks: trackingUrl.toJSON().clicks + 1 });
+      res.status(200).json(trackingUrl);
+    } else {
+      res
+        .status(404)
+        .json(`Tracking link with name ${req.params.name} not found.`);
+    }
+  } catch {
+    res.status(400).json({});
+  }
+});
+
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
